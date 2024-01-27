68e047da37c785e9deec4cfefd7d7ad69722e4d3
pwz857409056 <857409056@qq.com>
2024-01-27 21:10:28 +0800

    update

---
+++ src/Foundation/Support/Middlewares/RequestMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kindness\ModuleManage\Foundation\Support\Middlewares;
+
+use Webman\MiddlewareInterface;
+use Webman\Http\Response;
+use Webman\Http\Request;
+
+class RequestMiddleware implements MiddlewareInterface
+{
+    public function process(Request $request, callable $handler): Response
+    {
+        if ($request->plugin) {
+            app($request->plugin)['request'] = $request;
+        }
+        $response = $handler($request);
+        return $response;
+    }
+}
