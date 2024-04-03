45d75effae9c96548d882b4579ae68df9173dca6
Mr蒲 <857409056@qq.com>
2024-04-03 15:06:58 +0800

    update

--- src/Foundation/Support/Middlewares/RequestMiddleware.php
+++ src/Foundation/Support/Middlewares/RequestMiddleware.php
@@ -8,6 +8,13 @@ use Webman\Http\Request;
 
 class RequestMiddleware implements MiddlewareInterface
 {
+    /**
+     * @desc: 这个主要是为了解决laravel的数据库包操作分页时的问题
+     *
+     * @param Request $request
+     * @param callable $handler
+     * @return Response
+     */
     public function process(Request $request, callable $handler): Response
     {
         if ($request->plugin) {
